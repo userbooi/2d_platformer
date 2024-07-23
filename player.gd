ef7504f484d49a9afcf1c1d1b31cf4bef4f23f03
Edwin Wen <edwinwen75@gmail.com>
2024-07-23 12:55:38 -0400

    left and right movements

--- player.gd
+++ player.gd
@@ -1,24 +1,29 @@
 extends RigidBody2D
 
-@export var speed = 200
+@export var speed = 400
+@export var jump_force = 300
+var jump = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	position = Vector2(10, 10)
 	show()
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("move_right"):
-		velocity.x = 1
-	elif Input.is_action_pressed("move_left"):
-		velocity.x = -1
+	pass
+	#print(delta)
+	
+func _integrate_forces(state):
+	#print(state.get_linear_velocity())
+	var velocity = state.get_linear_velocity()
+	var step = state.get_step()
+	
+	
+	if Input.is_action_pressed("move_right") and velocity.x < 200:
+		velocity.x += speed * step * 20
+	elif Input.is_action_pressed("move_left") and velocity.x > -200:
+		velocity.x -= speed * step * 20
 		
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed * delta * 400
-		
-	apply_force(velocity)
-		
+	state.set_linear_velocity(velocity)	
 	
