c80df84c8d74f5cec654cf2c0faade25c9098e57
Edwin Wen <edwinwen75@gmail.com>
2024-07-23 13:51:01 -0400

    jumping

--- player.gd
+++ player.gd
@@ -1,8 +1,9 @@
 extends RigidBody2D
 
-@export var speed = 400
-@export var jump_force = 300
-var jump = false
+@export var ground_speed = 400
+@export var air_speed = 100
+@export var jump_force = 600
+var jumping = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,11 +20,25 @@ func _integrate_forces(state):
 	var velocity = state.get_linear_velocity()
 	var step = state.get_step()
 	
+	for x in range(state.get_contact_count()):
+		if state.get_contact_local_normal(x) == Vector2(0, -1):
+			#print("floor")
+			jumping = false
+			
+	if Input.is_action_pressed("jump") and !jumping:
+		velocity.y -= jump_force * step * 20
+		jumping = true
 	
-	if Input.is_action_pressed("move_right") and velocity.x < 200:
-		velocity.x += speed * step * 20
-	elif Input.is_action_pressed("move_left") and velocity.x > -200:
-		velocity.x -= speed * step * 20
+	if !jumping:
+		if Input.is_action_pressed("move_right") and velocity.x < 200:
+			velocity.x += ground_speed * step * 10
+		elif Input.is_action_pressed("move_left") and velocity.x > -200:
+			velocity.x -= ground_speed * step * 10
+	else:
+		if Input.is_action_pressed("move_right") and velocity.x < 200:
+			velocity.x += air_speed * step * 10
+		elif Input.is_action_pressed("move_left") and velocity.x > -200:
+			velocity.x -= air_speed * step * 10
 		
 	state.set_linear_velocity(velocity)	
 	
