f4bb1759172216be97c8f2ef1c73a138d2672bab
Edwin Wen <edwinwen75@gmail.com>
2024-08-02 12:12:59 -0400

    created door and animation for door

--- main_game.gd
+++ main_game.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
+@export var level = 1
+var door_positions = [Vector2(1105, 322)]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Player._move_to_position($Marker2D.position)
+	$Door.go_to_position(door_positions[0])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- player.gd
+++ player.gd
@@ -7,6 +7,7 @@ extends RigidBody2D
 var jumping = false
 var able_right = true
 var able_left = true
+var able_to_move = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -26,55 +27,55 @@ func _move_to_position(pos):
 
 func _integrate_forces(state):
 	#print(state.get_linear_velocity())
-	var velocity = state.get_linear_velocity()
-	var step = state.get_step()
-	var angle
-	var direction = Vector2.ZERO
-	
-	for x in range(state.get_contact_count()):
-		var local_normal = state.get_contact_local_normal(x)
-		var angle_between = acos(local_normal.dot(Vector2(0, -1))/(local_normal.length()))
-		#print(round(rad_to_deg(angle_between)))
-	
-		if 0 <= angle_between and angle_between < PI/2:
-			angle = angle_between
-			direction = Vector2(-local_normal.y, local_normal.x)
-			print(angle, direction)
-			jumping = false
-			if !able_right:
-				able_right = true
-			if !able_left:
-				able_left = true
-		elif state.get_contact_local_normal(x) == Vector2(-1, 0):
-			able_right = false
-		elif state.get_contact_local_normal(x) == Vector2(1, 0):
-			able_left = false
-			
-	if Input.is_action_pressed("jump") and !jumping:
-		velocity.y -= jump_force * step * 20
-		jumping = true
-	
-	if !jumping:
-		if angle != null and 0 <= angle and angle < PI/2:
-			if direction == Vector2(1, 0):
-				if Input.is_action_pressed("move_right") and velocity.x < 200:
-					velocity.x += ground_speed * step * 10
-				elif Input.is_action_pressed("move_left") and velocity.x > -200:
-					velocity.x -= ground_speed * step * 10
-			else:
-				if Input.is_action_pressed("move_left"):
-					direction *= -1
-				if Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right"):
-					velocity = direction.normalized() * slope_speed * step * 20
-	else:
-		if Input.is_action_pressed("move_right") and able_right and velocity.x < 200:
-			if velocity.x < 0:
-				velocity.x = 0
-			velocity.x += air_speed * step * 10
-		elif Input.is_action_pressed("move_left") and able_left and velocity.x > -200:
-			if velocity.x > 0:
-				velocity.x = 0
-			velocity.x -= air_speed * step * 10
+	if able_to_move:
+		var velocity = state.get_linear_velocity()
+		var step = state.get_step()
+		var angle
+		var direction = Vector2.ZERO
 		
-	state.set_linear_velocity(velocity)	
+		for x in range(state.get_contact_count()):
+			var local_normal = state.get_contact_local_normal(x)
+			var angle_between = acos(local_normal.dot(Vector2(0, -1))/(local_normal.length()))
+			#print(round(rad_to_deg(angle_between)))
+		
+			if 0 <= angle_between and angle_between < PI/2:
+				angle = angle_between
+				direction = Vector2(-local_normal.y, local_normal.x)
+				jumping = false
+				if !able_right:
+					able_right = true
+				if !able_left:
+					able_left = true
+			elif state.get_contact_local_normal(x) == Vector2(-1, 0):
+				able_right = false
+			elif state.get_contact_local_normal(x) == Vector2(1, 0):
+				able_left = false
+				
+		if Input.is_action_pressed("jump") and !jumping:
+			velocity.y -= jump_force * step * 20
+			jumping = true
+		
+		if !jumping:
+			if angle != null and 0 <= angle and angle < PI/2:
+				if direction == Vector2(1, 0):
+					if Input.is_action_pressed("move_right") and velocity.x < 200:
+						velocity.x += ground_speed * step * 10
+					elif Input.is_action_pressed("move_left") and velocity.x > -200:
+						velocity.x -= ground_speed * step * 10
+				else:
+					if Input.is_action_pressed("move_left"):
+						direction *= -1
+					if Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right"):
+						velocity = direction.normalized() * slope_speed * step * 20
+		else:
+			if Input.is_action_pressed("move_right") and able_right and velocity.x < 200:
+				if velocity.x < 0:
+					velocity.x = 0
+				velocity.x += air_speed * step * 10
+			elif Input.is_action_pressed("move_left") and able_left and velocity.x > -200:
+				if velocity.x > 0:
+					velocity.x = 0
+				velocity.x -= air_speed * step * 10
+			
+		state.set_linear_velocity(velocity)	
 	
