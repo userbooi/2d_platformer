07a34b0eba7bad00388cd7dba12c61cc1760751d
Edwin Wen <edwinwen75@gmail.com>
2024-08-04 18:38:26 -0400

    made level 4

--- main_game.gd
+++ main_game.gd
@@ -3,24 +3,34 @@ extends Node2D
 enum STATE {OPENING, PLAYING, DEATH, WAITING, WIN}
 
 @export var level = 1
-@export var levels = 3
+@export var levels = 4
 var door_positions = [
 	Vector2(1105, 322), 
 	Vector2(1065, 387), 
 	Vector2(1019, 192),
+	Vector2(470, -393),
 ] # subtract 68 to y value
 var player_positions = [
-	Vector2(60, 548), 
-	Vector2(69, 288),
-	Vector2(66, 418)
-] # subtract 37 to y value
+	Vector2(60, 560), 
+	Vector2(69, 290),
+	Vector2(66, 430),
+	Vector2(57, 40),
+] # subtract 25 to y value
 var start_game_position = Vector2(60, -1598)
 
 var game_state = STATE.OPENING
+#var game_state = STATE.PLAYING
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	begin()
+	#if !(int($Player.position.y) == player_positions[level-1].y):
+		#$Player.move_to_position(player_positions[level-1])
+	#$Player.set_gravity_scale(1)
+	#$Player.able_to_move = true
+	#$Door.go_to_position(door_positions[level-1])
+	#$Door.show()
+	#$Levels.show_level(level, levels)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -34,15 +44,14 @@ func start_game_cutscene():
 
 func begin():
 	$ColorRect.color = Color(0, 0, 0, 0)
-	level = 1
 	
 	if game_state == STATE.OPENING:
+		level = 1
 		$Player.set_gravity_scale(1)
 		$Player.move_to_position(start_game_position)
 		$Levels.show_level(level, levels)
 		$Door.hide()
 	else:
-		#print($Player.position)
 		if !(int($Player.position.y) == player_positions[level-1].y):
 			$Player.move_to_position(player_positions[level-1])
 		$Player.set_gravity_scale(1)
@@ -71,6 +80,7 @@ func set_up_level():
 	
 	$Player.move_to_position(player_positions[level-1])
 	$Door.go_to_position(door_positions[level-1])
+	$Door.show()
 	$Levels.show_level(level, levels)
 	
 	$AnimationPlayer.play("fade_out")
