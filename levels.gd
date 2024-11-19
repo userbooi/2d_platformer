dbc6a8bea130b14f25b932a3cb0809c76cec228b
Edwin Wen <edwinwen75@gmail.com>
2024-11-19 18:24:47 -0500

    final commit

--- levels.gd
+++ levels.gd
@@ -1,16 +1,15 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-	#for child in get_children():
-		#print(child.name)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	#var test_var = $level1/Path2D.curve.get_closest_offset(Vector2(400, 400))
+	#var length = $level1/Path2D.curve.get_baked_length()
+	#print(test_var/length)
 	
 func show_level(curr_level, levels):
 	for level in range(levels):
